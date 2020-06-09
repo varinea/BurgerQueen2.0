49834008296bc222f9e47196f1362ff78be0cc8b
JocelynJofreAntoine <joce.jofreantoine@gmail.com>
2020-06-08 21:00:45 -0400

    creación de componentes jsx y css

---
+++ src/components/Breakfast.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Breakfast.css';
+
+const Breakfast = () => {
+    return (
+        <div>
+            <button >Desayuno</button> 
+            <button >Almuerzo</button> 
+            <button >Cocina</button> 
+            
+        </div>
+    )
+}
+
+export default Breakfast
---
+++ src/components/Lunch.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Lunch.css';
+
+const Lunch = () => {
+    return (
+        <div>
+            <h1>almuerzo</h1>
+
+            <button >Desayuno</button> 
+            <button >Almuerzo</button>   
+        </div>
+    )
+}
+
+export default Lunch
