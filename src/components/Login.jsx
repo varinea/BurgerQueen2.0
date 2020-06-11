4da3fa514e50a79e8f31b5dc0141550f072d8156
camila-osores <camilaosoresa@gmail.com>
2020-06-11 19:18:33 -0400

    Insertando favicon, maquetación de vista breakfast

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import{Link} from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
             <h1>login</h1>
-            <button >Ingresar</button>
         </div>
     )
 }
